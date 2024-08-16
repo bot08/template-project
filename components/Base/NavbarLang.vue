16095446df1ae82fb3d27bab6d13f66112680c50
Oleg <scherbaoleg4@gmail.com>
2024-08-16 21:58:32 +0300

    use v-for for lang

--- components/Base/NavbarLang.vue
+++ components/Base/NavbarLang.vue
@@ -8,19 +8,13 @@
     </BaseVisualFeedback>
     <transition
       enter-active-class="transition ease-out duration-250"
-      enter-from-class="opacity-0 scale-75 -translate-y-10 translate-x-2"
+      enter-from-class="opacity-0 scale-75 -translate-y-8 translate-x-2"
       leave-active-class="transition ease-out duration-250"
-      leave-to-class="opacity-0 scale-75 -translate-y-10 translate-x-2"
+      leave-to-class="opacity-0 scale-75 -translate-y-8 translate-x-2"
     >
       <div v-if="isOpen" class="absolute right-0 w-40 p-4 mt-8 rounded-2xl shadow-lg bg-white dark:bg-zinc-700">
-        <BaseVisualFeedback>
-          <button @click="setLocale('en')" class="block">English</button>
-        </BaseVisualFeedback>
-        <BaseVisualFeedback>
-          <button @click="setLocale('uk')" class="block">Українська</button>
-        </BaseVisualFeedback>
-        <BaseVisualFeedback>
-          <button @click="setLocale('ja')" class="block">日本語</button>
+        <BaseVisualFeedback v-for="lang in languages" :key="lang.code">
+          <button @click="setLocale(lang.code)" class="block">{{ lang.name }}</button>
         </BaseVisualFeedback>
       </div>
     </transition>
@@ -34,6 +28,12 @@ const { setLocale } = useI18n()
 
 const isOpen = ref(false)
 const langContainer = ref(null)
+
+const languages = ref([
+  { code: 'en', name: 'English' },
+  { code: 'uk', name: 'Українська' },
+  { code: 'ja', name: '日本語' }
+])
 
 const handleClickOutside = (event) => {
   if (langContainer.value && !langContainer.value.contains(event.target)) {
