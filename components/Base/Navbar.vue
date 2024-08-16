f811eaece17db531f21d102649989e45b28e23b9
Oleg <scherbaoleg4@gmail.com>
2024-08-16 14:55:34 +0300

    typo fix

--- components/Base/Navbar.vue
+++ components/Base/Navbar.vue
@@ -33,14 +33,16 @@
               </BaseVisualFeedback>
             </Transition>
           </button>
-          <!-- Mobile menu btn -->
-          <button class="block md:hidden">
-            <BaseVisualFeedback>
-              <Bars4Icon class="h-8 w-8"/>
-            </BaseVisualFeedback>
-          </button>
-          <!-- PC lang -->
-          <BaseNavbarLang/>
+          <div>
+            <!-- Mobile menu btn -->
+            <button class="block md:hidden">
+              <BaseVisualFeedback>
+                <Bars4Icon class="h-8 w-8"/>
+              </BaseVisualFeedback>
+            </button>
+            <!-- PC lang btn -->
+            <BaseNavbarLang/>
+          </div>
         </div>
       </div>
     </div>
